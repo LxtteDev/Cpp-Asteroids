d57539a7eb4c6d4c5d5139adda6a252f4aa4a1a6
LxtteDev <scott.engelbrecht2007@gmail.com>
2023-02-01 15:41:41 -0800

    Added SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace sf;
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+
+	RenderWindow window(VideoMode(100, 100), "Test");
+    window.display();
 }
